78daf4a1f22e727b8bfed588cff9a0ae597f7877
Zmeecer Jarasevic <zmeecer@gmail.com>
2016-04-22 00:11:39 +0300

    fixed board, divided files

---
+++ board.js
@@ -0,0 +1,48 @@
+import React, {
+  Component,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+
+const {width, height} = require('Dimensions').get('window');
+const size = 8;
+var cellSize = Math.floor(width / size);
+var letterSize = Math.floor(cellSize * .75);
+
+export default class MatchThree extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        {[...Array(size)].map((x, xIndex) =>
+          [...Array(size)].map((y, yIndex) => (
+            <View key={xIndex*size+yIndex} style={[styles.tile, {left: cellSize * xIndex, top: cellSize * yIndex}]}>
+              <Text style={styles.letter}>{xIndex*size+yIndex}</Text>
+            </View>
+          ))
+        )}
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    width: cellSize * size,
+    height: cellSize * size,
+    backgroundColor: '#ddFCFF',
+  },
+  tile: {
+    position: 'absolute',
+    width: cellSize,
+    height: cellSize,
+    borderRadius: 10,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#BEE1D2',
+  },
+  letter: {
+    color: '#333',
+    fontSize: letterSize,
+  },
+});
--- main.js
+++ main.js
@@ -1,49 +1,28 @@
+'use strict';
+
 import React, {
-  Component,
   StyleSheet,
-  Text,
-  View
+  View,
 } from 'react-native';
+import Board from './board.js';
 
-const {width, height} = require('Dimensions').get('window');
-const size = 8;
-var cellSize = Math.floor(width / size);
-var letterSize = Math.floor(cellSize * .75);
-
-export default class MatchThree extends Component {
+var Main = React.createClass({
   render() {
     return (
       <View style={styles.container}>
-        {[...Array(size)].map((x, xIndex) =>
-          [...Array(size)].map((y, yIndex) =>
-            <View key={xIndex*size+yIndex}
-                  style={styles.tile, {left: cellSize * xIndex, top: cellSize * yIndex}}>
-              <Text style={styles.letter}>A</Text>
-            </View>
-          )
-        )}
+        <Board/>
       </View>
-    );
-  }
-}
-
-const styles = StyleSheet.create({
-  container: {
-    width: cellSize * size,
-    height: cellSize * size,
-    backgroundColor: '#F5FCFF',
-  },
-  tile: {
-    position: 'absolute',
-    width: cellSize,
-    height: cellSize,
-    borderRadius: 10,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#BEE1D2',
-  },
-  letter: {
-    color: '#333',
-    fontSize: letterSize,
+    )
   },
 });
+
+var styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#646B62',
+  },
+});
+
+module.exports = Main;
